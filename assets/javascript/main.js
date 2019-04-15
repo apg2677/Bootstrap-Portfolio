7480c17a7cefd6a3cb541be3636747eb1c00ee62
Amir P <apourrezaei@gmail.com>
2019-04-15 17:10:20 -0400

    Single Link working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,13 @@
-$("document").ready(function() {
-    alert("hello!");
-    
+portfolioLinks = ["https://apg2677.github.io/TriviaGame/"];
+var pIndex;
 
+$("document").ready(function() {
+   
+    
    
 });
+
+function GetProject(i) {
+   // alert(portfolioLinks[i]);
+    window.location.href = portfolioLinks[i];
+}
