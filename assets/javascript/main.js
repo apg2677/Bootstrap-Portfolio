b71ab0d06ceb301d57d39f3bb0dffa63f0edc443
Amir P <apourrezaei@gmail.com>
2019-05-15 18:59:38 -0400

    Updated Portfolio

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,6 @@
-portfolioLinks = ["https://apg2677.github.io/unit-4-game/", "https://apg2677.github.io/TriviaGame/", "https://apg2677.github.io/Psychic-Game/", "https://apg2677.github.io/GifTastic/"];
+portfolioLinks = ["https://apg2677.github.io/unit-4-game/", "https://apg2677.github.io/TriviaGame/", 
+"https://apg2677.github.io/Psychic-Game/", "https://apg2677.github.io/GifTastic/", 
+    "https://stark-anchorage-35995.herokuapp.com/", "https://guarded-spire-69237.herokuapp.com/"];
 var pIndex;
 
 $("document").ready(function() {
